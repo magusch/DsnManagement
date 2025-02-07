f7c7b8c5381db7f3973b7ff4ede291897c9da878
magusch <magusch@gmail.com>
2025-02-07 10:44:33 +0700

    version 0.2.16.4 - prepared_text issue

--- events/utils.py
+++ events/utils.py
@@ -160,8 +160,8 @@ def move_event_to_post(Events_model):
     for event in events:
         event_dict = model_to_dict(event, fields=event2post_list)
         # make post in transfering
-        ev = make_a_post_text(event_dict)
         event_dict['prepared_text'] = event_dict['post']
+        ev = make_a_post_text(event_dict)
         event_dict['post'] = ev['post']
         event_dict['place_id'] = ev['place'] if ev['place'] is not None else (event.place.id if event.place is not None else None)
         if 'main_category' in ev:
